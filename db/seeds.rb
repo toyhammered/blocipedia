b3693ddf1c356462b3de4518d38a79bdb7549889
toyhammered <daniel@rassiner.com>
2015-12-20 21:56:26 -0800

    completed devise user sign-in and sign-up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+1.times do
+  user = User.create!(
+    email: "daniel@rassiner.com",
+    password: "fakefake",
+    password_confirmation: "fakefake",
+    confirmed_at: Time.now
+  )
+
+end
+
+5.times do
+  user = User.create!(
+    email: Faker::Internet.safe_email,
+    password: "fakefake",
+    password_confirmation: "fakefake",
+    confirmed_at: Time.now
+  )
+
+end
+users = User.all
+
+
+
+puts "Seeding Finished."
+puts "#{User.count} users created."
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
+  pw = "password"
+
   factory :user do
-    
+    email { Faker::Internet.email }
+    password pw
+    password_confirmation pw
+    confirmed_at Time.now
   end
 
 end
