e9708a2420374bbf11524e89b9af003ae9080d67
toyhammered <daniel@rassiner.com>
2016-01-03 14:03:54 -0800

    updated seeds to use classes, easier to make wikis and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,59 @@
-1.times do
-  user = User.create!(
-    email: "daniel@rassiner.com",
-    password: "fakefake",
-    password_confirmation: "fakefake",
-    confirmed_at: Time.now,
-    role: 2
-  )
+class CreateSeeds
+  def new_user(amount, email, role)
+    @amount = amount
 
-end
+    @amount.times do
+      return if @amount <= 0
 
-1.times do
-  user = User.create!(
-    email: "fake@fake.com",
-    password: "fakefake",
-    password_confirmation: "fakefake",
-    confirmed_at: Time.now,
-  )
+      begin
+        User.create!(
+          email: email,
+          password: "fakefake",
+          password_confirmation: "fakefake",
+          confirmed_at: Time.now,
+          role: role
+        )
 
-end
-5.times do
-  user = User.create!(
-    email: Faker::Internet.safe_email,
-    password: "fakefake",
-    password_confirmation: "fakefake",
-    confirmed_at: Time.now
-  )
+      rescue ActiveRecord::RecordInvalid
+        email = Faker::Internet.safe_email
+        new_user(@amount - 1, email, role)
+      end
+    end
+  end
 
-end
+  def new_wiki(amount, users)
+    amount.times do
+      user = users.sample
+      Wiki.create!(
+        user: user,
+        title: Faker::Book.title,
+        body: Faker::Hipster.paragraph,
+        private: user.standard? ? false : [true,false].sample
+      )
+    end
+  end
+
+end # end of CreateUser class
+
+create = CreateSeeds.new
+
+# Users
+create.new_user(1,"daniel@rassiner.com", :admin)
+create.new_user(1,"admin@example.com", :admin)
+create.new_user(1,"premium@example.com", :premium)
+create.new_user(1,"standard@example.com", :standard)
+create.new_user(5, Faker::Internet.safe_email, :standard)
+
 users = User.all
 
-40.times do
-  wiki = Wiki.create!(
-    user: users.sample,
-    title: Faker::Book.title,
-    body: Faker::Hipster.paragraph,
-    private: [true,false].sample
-  )
-end
+
+# Wikis
+create.new_wiki(75, users)
 
 wikis = Wiki.all
 
 
-
+# Seeding Completed
 puts "Seeding Finished."
 puts "#{User.count} users created."
 puts "#{Wiki.count} wikis created."
