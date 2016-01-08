204f83a0def403d126e198a4dcd40fc30e0a9a20
toyhammered <daniel@rassiner.com>
2016-01-07 17:21:41 -0800

    added different page params for all wikis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
     WillPaginate.per_page = 4
     @user = User.find(params[:id])
 
-    @private_wikis = (WikiPolicy::Scope.new(@user, Wiki).my_private_wiki).paginate(page: params[:page]).order('updated_at DESC')
+    @private_wikis = (WikiPolicy::Scope.new(@user, Wiki).my_private_wiki).paginate(page: params[:private_page]).order('updated_at DESC')
 
-    @public_wikis = (WikiPolicy::Scope.new(@user, Wiki).my_public_wiki).paginate(page: params[:page]).order('updated_at DESC')
+    @public_wikis = (WikiPolicy::Scope.new(@user, Wiki).my_public_wiki).paginate(page: params[:public_page]).order('updated_at DESC')
 
-    @editor_wikis = (WikiPolicy::Scope.new(@user, Wiki).my_editor_wiki).paginate(page: params[:page])
+    @editor_wikis = (WikiPolicy::Scope.new(@user, Wiki).my_editor_wiki).paginate(page: params[:editor_page]).order('updated_at DESC')
 
   end
 end
